c8496ce6a3673f6fcb364d71eb04e31cbb9c88c0
Alex-it-work <Mr.Kirieiev@gmail.com>
2021-06-23 22:30:41 +0300

    Done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,60 @@
 "use strict";
+
+// Напишите функцию printNumbers(from, to, interval), которая последовательно выводит в консоль целые числа, начиная от from и заканчивая to, с интервалом между выводом соседних чисел в interval мс.
+// Реализуйте первый или *оба варианта решения:
+// - Используя setInterval.
+// - *Используя рекурсивный setTimeout (https://learn.javascript.ru/settimeout-setinterval#rekursivnyy-settimeout).
+
+function printNumbers(from = 0, to = 0, interval = 0) {
+  checkArgProperty(from, to, interval);
+  let counter = from;
+  const timerId = setInterval(() => {
+    console.log(`printNumbers ${counter}`);
+    if (counter === to) {
+      clearInterval(timerId);
+    }
+    counter++;
+  }, interval);
+}
+
+function printNumbersRec(from = 0, to = 0, interval = 0) {
+  checkArgProperty(from, to, interval);
+  let counter = from;
+  setTimeout(function go() {
+    console.log(`printNumbersRec ${counter}`);
+    if (counter < to) {
+      setTimeout(go, interval);
+    }
+    counter++;
+  }, interval);
+}
+
+try {
+  printNumbers(0, 10, 1000);
+  printNumbersRec(0, 10, 1000);
+} catch (error) {
+  console.error(error);
+}
+
+function checkArgProperty(from, to, interval) {
+  if (
+    typeof from !== "number" ||
+    typeof to !== "number" ||
+    typeof interval !== "number"
+  ) {
+    throw new TypeError("Arguments must be a numbers");
+  }
+  if (
+    !Number.isSafeInteger(from) ||
+    !Number.isSafeInteger(to) ||
+    !Number.isSafeInteger(interval) ||
+    Number.isNaN(from) ||
+    Number.isNaN(to) ||
+    Number.isNaN(interval)
+  ) {
+    throw new RangeError("Arguments must be a safe integer number");
+  }
+  if (from > to || interval < 0) {
+    throw new Error("Arguments must be : from <= to || interval >= 0");
+  }
+}
